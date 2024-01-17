98851e1cc6b6e7333b10494654390e50b570afb6
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-01-17 13:27:46 +0700

    #3 create routes, controller

---
+++ blog/src/app/controllers/SiteController.js
@@ -0,0 +1,14 @@
+class NewController {
+
+  // [GET] /
+  index(req, res) {
+    res.render('home')
+  }
+
+  // [GET] /search
+  search(req, res) {
+    res.render('search')
+  }
+}
+
+module.exports = new NewController
